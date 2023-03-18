ef74eb29e82f34db197137b6a5013e0e6f8243fd
easlern <easlern@gmail.com>
2023-03-18 11:43:37 -0400

    Got rid of the image loading loop, it was causing problems

--- resources.js
+++ resources.js
@@ -1,13 +1,2 @@
-let waitForLoad = (image) => {
-    console.log(`loading ${image.src}. . .`);
-    while (!image.complete) sleep(100).then(() => {});
-    console.log(`loaded ${image.src}`);
-}
-
-let brick = new Image(100,100);
-brick.src = "images/brick.png";
-waitForLoad(brick);
-
-let whiteBrick = new Image(100,100);
-whiteBrick.src = "images/brick-white.png";
-waitForLoad(whiteBrick);
+let brick = document.getElementById(`brick`);
+let whiteBrick = document.getElementById(`brick-white`);
